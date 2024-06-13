8a7913805c112a43796de1da53f6eb0ead984786
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-12 21:12:42 -0300

    complete class

--- App.tsx
+++ App.tsx
@@ -1,17 +1,61 @@
+import { useEffect, useState } from "react"
 import { StatusBar } from "expo-status-bar"
 import { Feather } from "@expo/vector-icons"
-import { Pressable, StyleSheet, TextInput, View } from "react-native"
+import Voice, { SpeechResultsEvent } from "@react-native-voice/voice"
+import { Pressable, StyleSheet, TextInput, View, LogBox } from "react-native"
+
+LogBox.ignoreLogs(["new NativeEventEmitter"])
 
 export default function App() {
+  const [search, setSearch] = useState("")
+  const [isListening, setIsListening] = useState(false)
+
+  function onSpeechResults({ value }: SpeechResultsEvent) {
+    const text = value ?? []
+
+    const formatText = text.join().replace(",", " ")
+
+    setSearch(formatText)
+  }
+
+  async function handleListening() {
+    try {
+      if (isListening) {
+        setIsListening(false)
+        await Voice.stop()
+      } else {
+        setSearch("")
+        setIsListening(true)
+        await Voice.start("pt-BR")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    Voice.onSpeechResults = onSpeechResults
+  }, [])
+
   return (
     <View style={styles.container}>
       <StatusBar style="dark" backgroundColor="transparent" translucent />
 
       <View style={styles.header}>
-        <TextInput style={styles.input} placeholder="Pesquisar..." />
+        <TextInput
+          style={styles.input}
+          placeholder={isListening ? "Gravando..." : "Pesquisar..."}
+          editable={!isListening}
+          value={search}
+          onChangeText={setSearch}
+        />
 
-        <Pressable style={styles.button}>
-          <Feather name="mic" color="#FFF" size={24} />
+        <Pressable style={styles.button} onPress={handleListening}>
+          <Feather
+            name={isListening ? "pause" : "mic"}
+            color="#FFF"
+            size={24}
+          />
         </Pressable>
       </View>
     </View>
